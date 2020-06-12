b93bafefdc9cfb509d71fafc99ff17ee34439d59
Paul Opiyo <paulopiyo777@gmail.com>
2020-06-12 16:23:03 -0500

    modify template files

--- mypoll/polls/views.py
+++ mypoll/polls/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Question
@@ -6,12 +7,16 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # raise an exception if the question with requested ID doesn't exist
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
